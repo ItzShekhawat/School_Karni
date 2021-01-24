20d630768e74506eeb4b4b5bc7b8163685b43791
ItzShekhawat <shekhawatsinghkarni@gmail.com>
2021-01-24 12:47:47 +0100

    php try

---
+++ Computer Science/Info_LAB/Web_code/Testing Files/00_Simple_HelloWorld/HelloWorld.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo("<h1>Hello, World</h1>");
+        echo("<h2>How was you day today</h2>");
+
+    ?>
+</body>
+</body>
+</html>
