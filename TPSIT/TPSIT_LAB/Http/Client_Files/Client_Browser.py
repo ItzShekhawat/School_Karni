16c96cda1b233cc108beb370d06bdab70b181e58
ItzShekhawat <shekhawatsinghkarni@gmail.com>
2021-02-01 15:29:14 +0100

    save

---
+++ TPSIT/TPSIT_LAB/Http/Client_Files/Client_Browser.py
@@ -0,0 +1,35 @@
+# Browser Client in Python 
+# Author : Karni Singh Shekhawat
+
+import socket as sock
+import  time
+import requests
+
+
+# Variables 
+ip_server = ""
+port = 5000
+full_Address = (ip_server, port)
+
+def connect_server():
+    clin = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
+    clin.connect(full_Address)
+
+class http.client.HTTPConnection(host, port=None, [timeout, ]source_address=None, blocksize=8192
+
+
+
+
+
+def send_request():
+   name = "Karni"
+   password = "Karni123"
+
+   
+
+
+
+
+
+
+
