00812d8c8244038d0afad732569f837ef923fbf2
ItzShekhawat <shekhawatsinghkarni@gmail.com>
2021-02-07 19:32:21 +0100

    Added Aphabot Class

--- TPSIT/TPSIT_LAB/Flask/School Apps/Aphabot_controll_page/app.py
+++ TPSIT/TPSIT_LAB/Flask/School Apps/Aphabot_controll_page/app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, request
+#########################################################################
+#                                                                       #
+# Author: Karni Singh Shekhawat                                         #
+# Date: 07/02/2021                                                      #
+# Description: This is a web server for the Alphabot controller page    #
+#                                                                       #
+#########################################################################
+
+from flask import Flask, render_template, request, url_for
 import logging
-import time
+from time import sleep
+# importing Alphabot form the Alphabot Class
+#from Alphabot import AlphaBot
 
-
+# Setting up the logging module
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(levelname)s:%(message)s')
 
 # Setting up the app 
 app = Flask(__name__)
+
+# Setting up an Alphabot runner
+#ap = AlphaBot()
 
 # Setting up the mail page load
 @app.route('/')
@@ -15,23 +29,35 @@ def index():
 # Setting up the forward runner
 @app.route('/forward')
 def forward():
-
+    logging.debug('Forwarding')
+    #ap.forward()
+    sleep(1)
     return render_template('index.html')
 
 # Setting up the backward runner
 @app.route('/backward')
 def backward():
-    return 'backward'
+    logging.debug('Going backward')
+    #ap.backward()
+    sleep(1)
+    return render_template('index.html')
 
 # Setting up the left turn
 @app.route('/left')
 def left():
-    return 'left'
+    logging.debug('Turning left')
+    #ap.left()
+    sleep(1)
+    sleep()
+    return render_template('index.html')
 
 # Setting up the right turn
 @app.route('/right')
 def right():
-    return 'right'
+    logging.debug('Turning right')
+    #ap.right()
+    sleep(1)
+    return render_template('index.html')
 
 
 
