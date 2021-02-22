0f9e5d75e3fcf768f2dffb832b175ecd0988acd3
itzshekhawat <karni.shekhawat@itiscuneo.eu>
2021-02-22 23:16:33 +0100

    Adding DB

--- TPSIT/TPSIT_LAB/Flask/Self_Lern/website/__init__.py
+++ TPSIT/TPSIT_LAB/Flask/Self_Lern/website/__init__.py
@@ -1,14 +1,26 @@
 from flask import Flask, app
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db= SQLAlchemy()
+DBNAME = 'database.db'
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'KARNISINGHSHEKHAWAT'
+    app.config['SQLALCHEMY_DATABASE_URL'] = f'sqlite:///{DBNAME}'
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
 
+
     app.register_blueprint(views, urlprefix='/')
     app.register_blueprint(auth, urlprefix='/')
+
 
     return app
 
--- TPSIT/TPSIT_LAB/Flask/Self_Lern/website/auth.py
+++ TPSIT/TPSIT_LAB/Flask/Self_Lern/website/auth.py
@@ -1,16 +1,35 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-    return "<p> Login you self </p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password1 = request.form.get('password')
+        #password2 = request.POST.get('password2')
 
-@auth.route('/logout')
+        if len(email) < 4:
+            flash("Email must be greater than 4 characters", category='error')
+            ##elif len(password1) != len(password2):
+            ##    flash("Password1 is not equal to password2", category='error') 
+        elif len(password1) < 7 :
+            flash("Password1 must be greater than 7 characters", category='error')
+        else:
+            flash("Login success",category='success')
+            # Adding to the DB
+
+    return render_template('login.html')
+
+
+
+
+
+@auth.route('/logout', methods=[ 'GET', 'POST' ])
 def logout():
-    return "<p> Logout </p>"
+    return render_template('logout.html')
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=[ 'GET ', 'POST' ])
 def sign():
-    return "<p> Sign Up </p>"
+    return render_template('signup.html')
     
