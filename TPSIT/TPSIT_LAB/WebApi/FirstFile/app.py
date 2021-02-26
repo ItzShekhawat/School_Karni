e2fdf92f15aa710872965dcfd9d147104bf46422
itzshekhawat <karni.shekhawat@itiscuneo.eu>
2021-02-26 09:05:46 +0100

    Some changing

--- TPSIT/TPSIT_LAB/WebApi/FirstFile/app.py
+++ TPSIT/TPSIT_LAB/WebApi/FirstFile/app.py
@@ -1,34 +1,26 @@
-import flask
+from flask import Flask, jsonify
 
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
 books = [
- {'id': 0, 'title': 'Il nome della Rosa',
- 'author': 'Umberto Eco',
- 'year_published': '1980'},
- {'id': 1,
- 'title': 'Il problema dei tre corpi',
- 'author': 'Liu Cixin',
- 'published': '2008'},
- {'id': 2,
- 'title': 'Fondazione',
- 'author': 'Isaac Asimov',
- 'published': '1951'}
-]
-
+ {'id': 0, 'title': 'Il nome della Rosa', 'author': 'Umberto Eco','year_published': '1980'},
+ {'id': 1, 'title': 'Il problema dei tre corpi', 'author': 'Liu Cixin', 'published': '2008'},
+ {'id': 2,'title': 'Fondazione', 'author': 'Isaac Asimov', 'published': '1951'} ]
 
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Biblioteca online</h1><p>Prototipo di web API.</p>"
-app.run()
 
-@app.route('/api', methods=['GET'])
+@app.route('/api')
 def api_all():
-    return flask.jsonify(books)
+    return jsonify(books)
+
+@app.route('/findip')
+def findip():
+
+    return "todo"
 
 
 if __name__ == '__main__':
-
-    app.run(host='127.0.0.1', debug=True)
-
+    app.run()
