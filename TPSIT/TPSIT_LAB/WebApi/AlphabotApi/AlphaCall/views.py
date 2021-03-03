0f3330cdde09e26afcff275d45097b56452dd9a2
itzshekhawat <karni.shekhawat@itiscuneo.eu>
2021-03-03 19:07:21 +0100

    DB query still not working properly

--- TPSIT/TPSIT_LAB/WebApi/AlphabotApi/AlphaCall/views.py
+++ TPSIT/TPSIT_LAB/WebApi/AlphabotApi/AlphaCall/views.py
@@ -1,6 +1,11 @@
 from flask import Flask, Blueprint, request, render_template, jsonify
+import sqlite3
 
 views = Blueprint('views', __name__)
+
+pathSTART = ""
+pathEND = ""
+path = ""
 
 # Home page route
 @views.route('/')
@@ -11,6 +16,9 @@ def index():
 #### Path Send  #### 
 @views.route('/path', methods=['POST'])
 def path():
+
+    """
+
     if request.method == 'POST':
         if 'pathSTART' in request.args:
             pathSTART = request.args['pathSTART']
@@ -19,7 +27,35 @@ def path():
         else:
             return "The path Start and End must be specified."
     
-    print (f"The starting point is {pathSTART} and the ending point is {pathEND}")
+    """
+
+    if request.method == 'POST':
+        pathSTART = request.form.get('start')
+        pathEND = request.form.get('stop')
+    else:
+        print ("Error")
+
+    # DB connection
+
+    db = sqlite3.connect('percosi.db')
+    cursor = db.cursor()
+    query = "SELECT percorso FROM percorsi INNER JOIN inizio_fine ON inizio_fine.id_percorso = percorsi.id WHERE (SELECT id_start FROM inizio_fine INNER JOIN luoghi WHERE luoghi.id = inizio_fine.id_start AND luoghi.nome = '{pathEND}') = inizio_fine.id_start AND(SELECT id_end FROM inizio_fine INNER JOIN luoghi WHERE luoghi.id = inizio_fine.id_end AND luoghi.nome = '{pathSTART}') = inizio_fine.id_end ;"
+    try:
+        cursor.execute(query)
+    except:
+        print ("Error")
     
+    try:
+        path = cursor.fetchall()
+        if path != None:
+            db.close()
+        else:   
+            print (f"The path is {path}")
+    except:
+         print ("Fetch didn't worked'")
+
+    return (f"The starting point is {pathSTART} and the ending point is {pathEND}")
+    
+
             
 
